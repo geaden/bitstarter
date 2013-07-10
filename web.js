804c1f6f34763692b05a5f97f35c716e50c9c52d
Gennady Denisov <denisovgena@gmail.com>
2013-07-10 20:27:36 +0000

    get rid of buffer

--- web.js
+++ web.js
@@ -3,12 +3,10 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(23);
-
-fs.readFileSync('index.html', buffer);
+var text = fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
-    response.send(buffer.toString());
+    response.send(text);
 });
 
 var port = process.env.PORT || 5000;
